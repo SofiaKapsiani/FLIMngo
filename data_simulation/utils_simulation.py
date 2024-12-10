fff5777fa152bf673447fabe478ef1397d3e9dbd
Sofia Kapsiani <77961877+SofiaKapsiani@users.noreply.github.com>
2024-12-10 17:33:43 +0000

    Add model training files

--- data_simulation/utils_simulation.py
+++ data_simulation/utils_simulation.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
+import torch
 from PIL import Image
 import sdtfile as sdt
+from skimage import transform
 
-# --- cropp_imgs.ipynb --- #
+# --- cropp_imgs.ipynb --- 
 def colour_channel_imgs(img_dir, img):
     '''Load and open each colour channel image
     red: microtubules,
@@ -14,7 +16,7 @@ def colour_channel_imgs(img_dir, img):
     return [Image.open(img_path+'_red.png'), Image.open(img_path+'_green.png'),
                 Image.open(img_path+'_blue.png'), Image.open(img_path+'_yellow.png')]
 
-# <--- irf_simulation.ipynb  -->
+# --- irf_simulation.ipynb --- 
 def load_sdt(data_dir, file_name):
     """ Read Becker & Hickl .sdt files"""
     sdt_file = sdt.SdtFile(os.path.join(data_dir, file_name))
@@ -44,4 +46,57 @@ def resample_array(arr, new_size):
         reduced_array[i] = np.mean(arr[start_index:end_index])
     return reduced_array
 
+# --- lifetime_simulation.ipynb ---
 
+def getPerlin(X,Y,octaves=5,persistence=0.6):
+    """
+    Function to generate perlin noise
+    Args:
+        X: The x dimension of the perlin noise array
+        Y: The y dimension of the perlin noise array
+        octaves: The number of octaves to be used in the perlin noise generation
+        persistence: The persistence to be used in the perlin noise generation
+
+    Returns:
+        perlin: The perlin noise array
+    """
+    perlin = np.zeros((X,Y,octaves)) # Initialize the numpy array
+
+    for i in range(octaves):
+        # Generate an x BY y array of random numbers between 0 and 1
+        x = int(X/2**i) # x = (np.floor()X/n)
+        y = int(Y/2**i) # x = (np.floor()X/n)
+        Z = np.random.rand(x,y)
+        # Upsample the array to X by Y
+        perlin[:,:,i] = transform.resize(Z, (X,Y))
+        perlin[:,:,i] = perlin[:,:,i]*persistence**(octaves-i)
+    # Plot the original and upsampled arrays
+
+    perlin = np.sum(perlin,2) # Sum the octaves
+    perlin = perlin-np.amin(perlin) # Normalize the data to 0 and 1
+    perlin = perlin/np.amax(perlin)
+
+    return perlin
+
+
+def tau_channel(img, dim =[256, 256], tau_range = [0.1, 10.0], tau_dist = [0.2, 2]):
+    '''Generates the range of tau observed in a single colour channel'''
+    tau = torch.tensor(np.random.uniform(tau_range[0], tau_range[1]))  # randomly assign a tau value for each colour channel
+    diff_range = torch.tensor(np.random.uniform(tau_dist[0], tau_dist[1]))  # randomly select the difference between the min and max tau observed in the same colour channel
+    tau_matrix = torch.rand(dim[0], dim[1]) * diff_range + tau  # matrix of 128 x 128 dim with random tau distribution
+    tau_img = torch.where(img == 0, torch.tensor(0.0), tau_matrix)  # assign tau values on non-background pixels of microscopy image
+    return tau_img
+
+
+def calculate_ratio(random_value, num_parameters):
+    '''Generates tau contribultion of each colour channel to the final lifetime image'''
+    ratio = []
+    remaining_ratio = 1.0
+
+    for i in range(num_parameters - 1):
+        contribution = random_value * remaining_ratio
+        ratio.append(contribution)
+        remaining_ratio -= contribution
+
+    ratio.append(remaining_ratio)
+    return ratio
---
+++ train_model/yolo_backbone/flimngo_net.py
@@ -0,0 +1,183 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.init
+import torch.nn.functional as F
+import torchvision
+import functools # used by RRDBNet
+from torchvision.transforms import Resize
+
+def GetModel(opt):
+    
+    if opt.model.lower() == 'flimngo':
+        net = FLIMngo(opt)
+    else:
+        print("model undefined")    
+        return None
+    
+    if not opt.cpu:
+        net.cuda()
+
+    return net
+
+## Yolo Network for zernike (RCAN_zernike)
+class CBL(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(CBL, self).__init__()
+
+        conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        bn = nn.BatchNorm2d(out_channels)
+
+        self.cbl = nn.Sequential(
+            conv,
+            bn,
+            
+            nn.SiLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.cbl(x)
+    
+class Bottleneck(nn.Module):
+    """
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        width_multiple (float): it controls the number of channels (and weights)
+                                of all the convolutions beside the
+                                first and last one. If closer to 0,
+                                the simpler the modelIf closer to 1,
+                                the model becomes more complex
+    """
+    def __init__(self, in_channels, out_channels, width_multiple=1):
+        super(Bottleneck, self).__init__()
+        c_ = int(width_multiple*in_channels)
+        self.c1 = CBL(in_channels, c_, kernel_size=1, stride=1, padding=0)
+        self.c2 = CBL(c_, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        return self.c2(self.c1(x)) + x
+
+class C3(nn.Module):
+    """
+    Parameters:
+        in_channels (int): number of channel of the input tensor
+        out_channels (int): number of channel of the output tensor
+        width_multiple (float): it controls the number of channels (and weights)
+                                of all the convolutions beside the
+                                first and last one. If closer to 0,
+                                the simpler the modelIf closer to 1,
+                                the model becomes more complex
+        depth (int): it controls the number of times the bottleneck (residual block)
+                        is repeated within the C3 block
+        backbone (bool): if True, self.seq will be composed by bottlenecks 1, if False
+                            it will be composed by bottlenecks 2 (check in the image linked below)
+        https://user-images.githubusercontent.com/31005897/172404576-c260dcf9-76bb-4bc8-b6a9-f2d987792583.png
+    """
+    def __init__(self, in_channels, out_channels, width_multiple=1, depth=1, backbone=True):
+        super(C3, self).__init__()
+        c_ = int(width_multiple*in_channels)
+
+        self.c1 = CBL(in_channels, c_, kernel_size=1, stride=1, padding=0)
+        self.c_skipped = CBL(in_channels,  c_, kernel_size=1, stride=1, padding=0)
+        if backbone:
+            self.seq = nn.Sequential(
+                *[Bottleneck(c_, c_, width_multiple=1) for _ in range(depth)]
+            )
+        else:
+            self.seq = nn.Sequential(
+                *[nn.Sequential(
+                    CBL(c_, c_, 1, 1, 0),
+                    CBL(c_, c_, 3, 1, 1)
+                ) for _ in range(depth)]
+            )
+        self.c_out = CBL(c_ * 2, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        x = torch.cat([self.seq(self.c1(x)), self.c_skipped(x)], dim=1)
+        return self.c_out(x)   
+
+class SPPF(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(SPPF, self).__init__()
+
+        c_ = int(in_channels//2)
+
+        self.c1 = CBL(in_channels, c_, 1, 1, 0)
+        self.pool = nn.MaxPool2d(kernel_size=5, stride=1, padding=2)
+        self.c_out = CBL(c_ * 4, out_channels, 1, 1, 0)
+
+    def forward(self, x):
+        x = self.c1(x)
+        pool1 = self.pool(x)
+        pool2 = self.pool(pool1)
+        pool3 = self.pool(pool2)
+
+        return self.c_out(torch.cat([x, pool1, pool2, pool3], dim=1))
+
+
+class FLIMngo(nn.Module):
+    def __init__(self, opt):
+        super(FLIMngo, self).__init__()
+        n_in_channels = opt.n_in_channels
+        self.width_multiple = opt.width_multiple
+        self.first_out = 86
+        im_size = opt.imageSize
+
+        # Define backbone module
+        self.head = nn.Sequential(
+            CBL(n_in_channels, 2*self.first_out, kernel_size=5, stride=1, padding=2),
+            CBL(2*self.first_out, 2*self.first_out, kernel_size=3, stride=2, padding=1)
+        )
+
+        self.down_one = nn.Sequential(
+            C3(in_channels=2*self.first_out, out_channels=2*self.first_out, width_multiple=self.width_multiple, depth=4),
+            CBL(2*self.first_out, 4*self.first_out, kernel_size=3, stride=2, padding=1)
+        )
+
+        self.down_two = nn.Sequential(
+            C3(in_channels=4*self.first_out, out_channels=4*self.first_out, width_multiple=self.width_multiple, depth=4),
+            CBL(4*self.first_out, 8*self.first_out, kernel_size=3, stride=2, padding=1),
+            SPPF(8*self.first_out, 8*self.first_out)
+        )
+
+        self.up_one = nn.Sequential(
+            CBL(8*self.first_out, 4*self.first_out, kernel_size=1, stride=1, padding=0),
+            Resize((int(im_size/4), int(im_size/4)))
+        )
+
+        self.up_two = nn.Sequential(
+            CBL(8*self.first_out, 4*self.first_out, kernel_size=1, stride=1, padding=0),
+            C3(in_channels=4*self.first_out, out_channels=4*self.first_out, width_multiple=self.width_multiple, depth=4),
+            CBL(4*self.first_out, 2*self.first_out, kernel_size=1, stride=1, padding=0),
+            Resize((int(im_size/2), int(im_size/2)))
+        )
+
+        self.tail = nn.Sequential(
+            CBL(4*self.first_out, 2*self.first_out, kernel_size=1, stride=1, padding=0),
+            C3(in_channels=2*self.first_out, out_channels=2*self.first_out, width_multiple=self.width_multiple, depth=4),
+            Resize((int(im_size), int(im_size))),
+            CBL(2*self.first_out, self.first_out, kernel_size=1, stride=1, padding=0),
+            C3(in_channels=self.first_out, out_channels=self.first_out, width_multiple=self.width_multiple, depth=2),
+            nn.Conv2d(in_channels=self.first_out, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        """Through encoder, then decoder by adding U-skip connections. """
+        # x = [256 256 1]
+
+        # Encoder
+        pool1 = self.head(x) # [128 128 128]
+        pool2 = self.down_one(pool1) # [64 64 256]
+        pool3 = self.down_two(pool2) # [32 32 512]
+
+        # Decoder
+        concat_1 = torch.cat([self.up_one(pool3), pool2], dim=1) # [64 64 512]
+        concat_2 = torch.cat([self.up_two(concat_1), pool1], dim=1) # [128 128 256
+        
+        # Final activation
+        output = self.tail(concat_2)  # [B, 1, H, W]
+
+        return output.squeeze()  # Remove channel dimension if needed
+   
